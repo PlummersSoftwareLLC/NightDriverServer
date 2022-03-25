bcea0e571018299df7706ec05978e64f22af9504
Rutger van Bergen <rbergen@xs4all.nl>
2022-03-25 18:34:18 +0100

    initial CI setup

--- make_docker_arm64.sh
+++
@@ -1,8 +0,0 @@
-git pull
-docker build . -t nightdriver -f Dockerfile.arm64
-docker tag nightdriver davepl/nightdriverserver:arm64
-echo "Enter the password for davepl\'s Docker Hub:"
-docker login -u davepl
-docker push davepl/nightdriverserver:arm64
-
-
--- make_docker_x86.sh
+++
@@ -1,7 +0,0 @@
-git pull
-docker build . -t nightdriver -f Dockerfile.x86
-docker tag nightdriver davepl/nightdriverserver:latest
-echo "Enter the password for davepl\'s Docker Hub:"
-docker login -u davepl 
-docker push davepl/nightdriverserver:latest
-
